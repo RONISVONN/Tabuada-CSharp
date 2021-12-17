761261612abbb5d55d12a4f328405ac44e853e3d
RONISVONN GOMES DA SILVA <ronisvonn@hotmail.com>
2021-12-17 01:06:18 -0300

    Projeto Inicial

---
+++ Tabuada-CSharp/Tabuada-CSharp/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Tabuada_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo02 = new System.Windows.Forms.Label();
+            this.lblTitulo01 = new System.Windows.Forms.Label();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn03 = new System.Windows.Forms.Button();
+            this.btn04 = new System.Windows.Forms.Button();
+            this.btn08 = new System.Windows.Forms.Button();
+            this.btn07 = new System.Windows.Forms.Button();
+            this.btn06 = new System.Windows.Forms.Button();
+            this.btn05 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn09 = new System.Windows.Forms.Button();
+            this.lblTituloTabuada = new System.Windows.Forms.Label();
+            this.lblExibirTabuada = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo02
+            // 
+            this.lblTitulo02.AutoSize = true;
+            this.lblTitulo02.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo02.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo02.Location = new System.Drawing.Point(256, 61);
+            this.lblTitulo02.Name = "lblTitulo02";
+            this.lblTitulo02.Size = new System.Drawing.Size(182, 20);
+            this.lblTitulo02.TabIndex = 0;
+            this.lblTitulo02.Text = "Que deseja Consultar";
+            // 
+            // lblTitulo01
+            // 
+            this.lblTitulo01.AutoSize = true;
+            this.lblTitulo01.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo01.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo01.Location = new System.Drawing.Point(265, 23);
+            this.lblTitulo01.Name = "lblTitulo01";
+            this.lblTitulo01.Size = new System.Drawing.Size(163, 20);
+            this.lblTitulo01.TabIndex = 0;
+            this.lblTitulo01.Text = "Escolha a Tabuada";
+            // 
+            // btn01
+            // 
+            this.btn01.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btn01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn01.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn01.ForeColor = System.Drawing.Color.White;
+            this.btn01.Location = new System.Drawing.Point(258, 123);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(56, 56);
+            this.btn01.TabIndex = 4;
+            this.btn01.Text = "1";
+            this.btn01.UseVisualStyleBackColor = false;
+            this.btn01.Click += new System.EventHandler(this.btn01_Click);
+            this.btn01.MouseEnter += new System.EventHandler(this.btn01_MouseEnter);
+            this.btn01.MouseLeave += new System.EventHandler(this.btn01_MouseLeave);
+            // 
+            // btn02
+            // 
+            this.btn02.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btn02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn02.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn02.ForeColor = System.Drawing.Color.White;
+            this.btn02.Location = new System.Drawing.Point(321, 123);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(56, 56);
+            this.btn02.TabIndex = 5;
+            this.btn02.Text = "2";
+            this.btn02.UseVisualStyleBackColor = false;
+            this.btn02.Click += new System.EventHandler(this.btn02_Click);
+            this.btn02.MouseEnter += new System.EventHandler(this.btn02_MouseEnter);
+            this.btn02.MouseLeave += new System.EventHandler(this.btn02_MouseLeave);
+            // 
+            // btn03
+            // 
+            this.btn03.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btn03.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn03.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn03.ForeColor = System.Drawing.Color.White;
+            this.btn03.Location = new System.Drawing.Point(383, 123);
+            this.btn03.Name = "btn03";
+            this.btn03.Size = new System.Drawing.Size(56, 56);
+            this.btn03.TabIndex = 6;
+            this.btn03.Text = "3";
+            this.btn03.UseVisualStyleBackColor = false;
+            this.btn03.Click += new System.EventHandler(this.btn03_Click);
+            this.btn03.MouseEnter += new System.EventHandler(this.btn03_MouseEnter);
+            this.btn03.MouseLeave += new System.EventHandler(this.btn03_MouseLeave);
+            // 
+            // btn04
+            // 
+            this.btn04.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btn04.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn04.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn04.ForeColor = System.Drawing.Color.White;
+            this.btn04.Location = new System.Drawing.Point(258, 185);
+            this.btn04.Name = "btn04";
+            this.btn04.Size = new System.Drawing.Size(56, 56);
+            this.btn04.TabIndex = 7;
+            this.btn04.Text = "4";
+            this.btn04.UseVisualStyleBackColor = false;
+            this.btn04.Click += new System.EventHandler(this.btn04_Click);
+            this.btn04.MouseEnter += new System.EventHandler(this.btn04_MouseEnter);
+            this.btn04.MouseLeave += new System.EventHandler(this.btn04_MouseLeave);
+            // 
+            // btn08
+            // 
+            this.btn08.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btn08.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn08.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn08.ForeColor = System.Drawing.Color.White;
+            this.btn08.Location = new System.Drawing.Point(321, 247);
+            this.btn08.Name = "btn08";
+            this.btn08.Size = new System.Drawing.Size(56, 56);
+            this.btn08.TabIndex = 11;
+            this.btn08.Text = "8";
+            this.btn08.UseVisualStyleBackColor = false;
+            this.btn08.Click += new System.EventHandler(this.btn08_Click);
+            this.btn08.MouseEnter += new System.EventHandler(this.btn08_MouseEnter);
+            this.btn08.MouseLeave += new System.EventHandler(this.btn08_MouseLeave);
+            // 
+            // btn07
+            // 
+            this.btn07.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btn07.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn07.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn07.ForeColor = System.Drawing.Color.White;
+            this.btn07.Location = new System.Drawing.Point(258, 247);
+            this.btn07.Name = "btn07";
+            this.btn07.Size = new System.Drawing.Size(56, 56);
+            this.btn07.TabIndex = 10;
+            this.btn07.Text = "7";
+            this.btn07.UseVisualStyleBackColor = false;
+            this.btn07.Click += new System.EventHandler(this.btn07_Click);
+            this.btn07.MouseEnter += new System.EventHandler(this.btn07_MouseEnter);
+            this.btn07.MouseLeave += new System.EventHandler(this.btn07_MouseLeave);
+            // 
+            // btn06
+            // 
+            this.btn06.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btn06.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn06.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn06.ForeColor = System.Drawing.Color.White;
+            this.btn06.Location = new System.Drawing.Point(383, 185);
+            this.btn06.Name = "btn06";
+            this.btn06.Size = new System.Drawing.Size(56, 56);
+            this.btn06.TabIndex = 9;
+            this.btn06.Text = "6";
+            this.btn06.UseVisualStyleBackColor = false;
+            this.btn06.Click += new System.EventHandler(this.btn06_Click);
+            this.btn06.MouseEnter += new System.EventHandler(this.btn06_MouseEnter);
+            this.btn06.MouseLeave += new System.EventHandler(this.btn06_MouseLeave);
+            // 
+            // btn05
+            // 
+            this.btn05.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btn05.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn05.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn05.ForeColor = System.Drawing.Color.White;
+            this.btn05.Location = new System.Drawing.Point(321, 185);
+            this.btn05.Name = "btn05";
+            this.btn05.Size = new System.Drawing.Size(56, 56);
+            this.btn05.TabIndex = 8;
+            this.btn05.Text = "5";
+            this.btn05.UseVisualStyleBackColor = false;
+            this.btn05.Click += new System.EventHandler(this.btn05_Click);
+            this.btn05.MouseEnter += new System.EventHandler(this.btn05_MouseEnter);
+            this.btn05.MouseLeave += new System.EventHandler(this.btn05_MouseLeave);
+            // 
+            // btn10
+            // 
+            this.btn10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btn10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn10.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn10.ForeColor = System.Drawing.Color.White;
+            this.btn10.Location = new System.Drawing.Point(321, 309);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(56, 56);
+            this.btn10.TabIndex = 13;
+            this.btn10.Text = "10";
+            this.btn10.UseVisualStyleBackColor = false;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            this.btn10.MouseEnter += new System.EventHandler(this.btn10_MouseEnter);
+            this.btn10.MouseLeave += new System.EventHandler(this.btn10_MouseLeave);
+            // 
+            // btn09
+            // 
+            this.btn09.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btn09.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn09.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn09.ForeColor = System.Drawing.Color.White;
+            this.btn09.Location = new System.Drawing.Point(383, 247);
+            this.btn09.Name = "btn09";
+            this.btn09.Size = new System.Drawing.Size(56, 56);
+            this.btn09.TabIndex = 12;
+            this.btn09.Text = "9";
+            this.btn09.UseVisualStyleBackColor = false;
+            this.btn09.Click += new System.EventHandler(this.btn09_Click);
+            this.btn09.MouseEnter += new System.EventHandler(this.btn09_MouseEnter);
+            this.btn09.MouseLeave += new System.EventHandler(this.btn09_MouseLeave);
+            // 
+            // lblTituloTabuada
+            // 
+            this.lblTituloTabuada.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(88)))), ((int)(((byte)(162)))));
+            this.lblTituloTabuada.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTituloTabuada.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloTabuada.ForeColor = System.Drawing.Color.White;
+            this.lblTituloTabuada.Location = new System.Drawing.Point(19, 23);
+            this.lblTituloTabuada.Name = "lblTituloTabuada";
+            this.lblTituloTabuada.Size = new System.Drawing.Size(216, 50);
+            this.lblTituloTabuada.TabIndex = 118;
+            this.lblTituloTabuada.Text = "TABUADA";
+            this.lblTituloTabuada.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblExibirTabuada
+            // 
+            this.lblExibirTabuada.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(182)))), ((int)(((byte)(133)))));
+            this.lblExibirTabuada.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lblExibirTabuada.ForeColor = System.Drawing.Color.Black;
+            this.lblExibirTabuada.Location = new System.Drawing.Point(19, 73);
+            this.lblExibirTabuada.Name = "lblExibirTabuada";
+            this.lblExibirTabuada.Size = new System.Drawing.Size(216, 314);
+            this.lblExibirTabuada.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(454, 407);
+            this.Controls.Add(this.lblExibirTabuada);
+            this.Controls.Add(this.lblTituloTabuada);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn09);
+            this.Controls.Add(this.btn08);
+            this.Controls.Add(this.btn07);
+            this.Controls.Add(this.btn06);
+            this.Controls.Add(this.btn05);
+            this.Controls.Add(this.btn04);
+            this.Controls.Add(this.btn03);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.lblTitulo02);
+            this.Controls.Add(this.lblTitulo01);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTitulo02;
+        private System.Windows.Forms.Label lblTitulo01;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn03;
+        private System.Windows.Forms.Button btn04;
+        private System.Windows.Forms.Button btn08;
+        private System.Windows.Forms.Button btn07;
+        private System.Windows.Forms.Button btn06;
+        private System.Windows.Forms.Button btn05;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn09;
+        protected System.Windows.Forms.Label lblTituloTabuada;
+        private System.Windows.Forms.Label lblExibirTabuada;
+
+    }
+}
+
---
+++ Tabuada-CSharp/Tabuada-CSharp/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabuada_CSharp
+{
+    public partial class Form1 : Form
+    {
+
+        //Variável static global -> tabuadaEscolhida
+        static int tabuadaEscolhida = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //Metodo para Criar a Tabuada passando um valor como parametro
+        private String CriarTabuada(int tabuada)
+        {
+
+            //Variável resultado
+            String resultado = "";
+
+            //qtdMax 
+            //Contador
+            //Calculo
+            int qtdMax = 10, contador, calculo;
+
+            //Se tabuadaEscolhida diferente ou igual 0 Então faça
+            if (tabuadaEscolhida != 0)
+            {
+                for (contador = 1; contador <= qtdMax; contador++)
+                {
+                    calculo = tabuada * contador;
+
+                    //Environment.NewLine = Obtém a cadeia de caracteres de nova linha ou quebra de linha.
+                    resultado = resultado + tabuada + " X " + contador + " = " + calculo + Environment.NewLine;
+                }
+            }
+            //Se não faça
+            else
+            {
+                resultado = "";
+            }
+
+            return resultado;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //Metodo para Exibir a Tabuada passando um valor como parametro
+        public void ExibirTabuada(int valor)
+        {
+            lblExibirTabuada.Text = "";
+            tabuadaEscolhida = valor;
+            lblExibirTabuada.Text = CriarTabuada(tabuadaEscolhida);
+            lblTituloTabuada.Text = "TABUADA " + tabuadaEscolhida;
+        }
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //Evento ao Clicar
+        //No botão btn01 até btn10
+
+        //Botão Btn01 = (Tabuada 1)
+        private void btn01_Click(object sender, EventArgs e)
+        {
+            ExibirTabuada(1);
+        }
+
+        //Botão Btn02 = (Tabuada 2)
+        private void btn02_Click(object sender, EventArgs e)
+        {
+            ExibirTabuada(2);
+        }
+
+        //Botão Btn03 = (Tabuada 3)
+        private void btn03_Click(object sender, EventArgs e)
+        {
+            ExibirTabuada(3);
+        }
+
+        //Botão Btn04 = (Tabuada 4)
+        private void btn04_Click(object sender, EventArgs e)
+        {
+            ExibirTabuada(4);
+        }
+
+        //Botão Btn05 = (Tabuada 5)
+        private void btn05_Click(object sender, EventArgs e)
+        {
+            ExibirTabuada(5);
+        }
+
+        //Botão Btn06 = (Tabuada 6)
+        private void btn06_Click(object sender, EventArgs e)
+        {
+            ExibirTabuada(6);
+        }
+
+        //Botão Btn07 = (Tabuada 7)
+        private void btn07_Click(object sender, EventArgs e)
+        {
+            ExibirTabuada(7);
+        }
+
+        //Botão Btn08 = (Tabuada 8)
+        private void btn08_Click(object sender, EventArgs e)
+        {
+            ExibirTabuada(8);
+        }
+
+        //Botão Btn09 = (Tabuada 9)
+        private void btn09_Click(object sender, EventArgs e)
+        {
+            ExibirTabuada(9);
+        }
+
+        //Botão Btn10 = (Tabuada 10)
+        private void btn10_Click(object sender, EventArgs e)
+        {
+            ExibirTabuada(10);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //Metodo exibir (Cursor da Mao) = Cursors.Hand
+        //Ao entrar, sair de um btn = (Button)
+        private void CursorMao(String valor, Button botao)
+        {
+            if (valor.Equals("SIM"))
+            {
+                //Ao entrar com o maouse = Cursor da mão
+                this.Cursor = Cursors.Hand;
+                botao.BackColor = Color.FromArgb(255, 182, 133);
+                botao.Font = new Font("Microsoft Sans", 20.20F, System.Drawing.FontStyle.Bold);
+                //Etc...
+            }
+            else
+            {
+                //Ao Sair com o maouse = Curso padrão
+                this.Cursor = Cursors.Default;
+                botao.BackColor = Color.FromArgb(48, 48, 48);
+                botao.Font = new Font("Microsoft Sans", 18F, System.Drawing.FontStyle.Bold);
+                //Etc...
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //Botão Btn01 
+        private void btn01_MouseEnter(object sender, EventArgs e)
+        {
+            CursorMao("SIM", btn01);
+        }
+
+        private void btn01_MouseLeave(object sender, EventArgs e)
+        {
+            CursorMao("NAO", btn01);
+        }
+        /////////////////////////////////////////////////////////
+        //Botão Btn02 
+        private void btn02_MouseEnter(object sender, EventArgs e)
+        {
+            CursorMao("SIM", btn02);
+        }
+
+        private void btn02_MouseLeave(object sender, EventArgs e)
+        {
+            CursorMao("NAO", btn02);
+        }
+        /////////////////////////////////////////////////////////
+        //Botão Btn03
+        private void btn03_MouseEnter(object sender, EventArgs e)
+        {
+            CursorMao("SIM", btn03);
+        }
+
+        private void btn03_MouseLeave(object sender, EventArgs e)
+        {
+            CursorMao("NAO", btn03);
+        }
+        /////////////////////////////////////////////////////////
+        //Botão Btn04
+        private void btn04_MouseEnter(object sender, EventArgs e)
+        {
+            CursorMao("SIM", btn04);
+        }
+
+        private void btn04_MouseLeave(object sender, EventArgs e)
+        {
+            CursorMao("NAO", btn04);
+        }
+        /////////////////////////////////////////////////////////
+        //Botão Btn05
+        private void btn05_MouseEnter(object sender, EventArgs e)
+        {
+            CursorMao("SIM", btn05);
+        }
+
+        private void btn05_MouseLeave(object sender, EventArgs e)
+        {
+            CursorMao("NAO", btn05);
+        }
+        /////////////////////////////////////////////////////////
+        //Botão Btn06
+        private void btn06_MouseEnter(object sender, EventArgs e)
+        {
+            CursorMao("SIM", btn06);
+        }
+
+        private void btn06_MouseLeave(object sender, EventArgs e)
+        {
+            CursorMao("NAO", btn06);
+        }
+        /////////////////////////////////////////////////////////
+        //Botão Btn07
+        private void btn07_MouseEnter(object sender, EventArgs e)
+        {
+            CursorMao("SIM", btn07);
+        }
+
+        private void btn07_MouseLeave(object sender, EventArgs e)
+        {
+            CursorMao("NAO", btn07);
+        }
+        /////////////////////////////////////////////////////////
+        //Botão Btn08
+        private void btn08_MouseEnter(object sender, EventArgs e)
+        {
+            CursorMao("SIM", btn08);
+        }
+
+        private void btn08_MouseLeave(object sender, EventArgs e)
+        {
+            CursorMao("NAO", btn08);
+        }
+        /////////////////////////////////////////////////////////
+        //Botão Btn09
+        private void btn09_MouseEnter(object sender, EventArgs e)
+        {
+            CursorMao("SIM", btn09);
+        }
+
+        private void btn09_MouseLeave(object sender, EventArgs e)
+        {
+            CursorMao("NAO", btn09);
+        }
+        /////////////////////////////////////////////////////////
+        //Botão Btn10
+        private void btn10_MouseEnter(object sender, EventArgs e)
+        {
+            CursorMao("SIM", btn10);
+        }
+
+        private void btn10_MouseLeave(object sender, EventArgs e)
+        {
+            CursorMao("NAO", btn10);
+        }
+
+        //FIM
+    }
+}
